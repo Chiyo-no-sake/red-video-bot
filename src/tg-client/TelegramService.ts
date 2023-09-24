f54f6df9ddc5744afdc2d0eda35b471fffa83f4a
Luca Pasini <luca.pasini.26@gmail.com>
2023-09-24 20:20:49 +0200

    auth fix

--- src/tg-client/TelegramService.ts
+++ src/tg-client/TelegramService.ts
@@ -1,41 +1,60 @@
 import { TelegramClient } from "telegram";
 import { TelegramServiceConfig } from "./TelegramServiceConfig.js";
-import { StoreSession } from "telegram/sessions/index.js";
+import { StringSession } from "telegram/sessions/index.js";
 import { createInterface } from 'node:readline/promises'
 import { stdin, stdout } from 'process'
 import { BigInteger } from "big-integer";
 import { delay } from "../utils.js";
-import { Chat } from "grammy/types";
 import { EntityLike } from "telegram/define.js";
+import { readFileSync, writeFileSync  } from "fs";
+import path from "path";
 
 export default class TelegramService {
-    client: TelegramClient;
+    private client: TelegramClient | undefined;
     rl: any;
-    session: StoreSession;
+    private session: StringSession| undefined;
 
-    constructor(config: TelegramServiceConfig) {
-        this.session = new StoreSession(config.configDir + '/session');
+    constructor(private readonly config: TelegramServiceConfig) {
+    }
+
+    async initializeSession(phone?: string) {
+        this.session = new StringSession();
         this.rl = createInterface({ input: stdin, output: stdout })
 
         this.client = new TelegramClient(
             this.session,
-            config.apiId,
-            config.apiHash,
+            this.config.apiId,
+            this.config.apiHash,
             { connectionRetries: 5 }
         );
-    }
 
-    async initializeSession(phone?: string) {
         await this.client.start({
             phoneNumber: async () => phone || await this.rl.question('Insert phone number: '),
             phoneCode: async () => await this.rl.question('Insert received phone code: '),
             onError: (err) => console.log(err),
         })
 
-        this.session.save()
+        const str = this.session.save()
+        
+        // write string to file
+        const targetPath = path.join(this.config.configDir, "session.txt")
+        writeFileSync(targetPath, str)
     }
 
     async restoreSession(phone?: string) {
+        const targetPath = path.join(this.config.configDir, "session.txt")
+        const str = readFileSync(targetPath, 'utf-8')
+
+        this.session = new StringSession(str);
+        this.rl = createInterface({ input: stdin, output: stdout })
+
+        this.client = new TelegramClient(
+            this.session,
+            this.config.apiId,
+            this.config.apiHash,
+            { connectionRetries: 5 }
+        );
+
         await this.client.start({
             phoneCode: async () => {
                 throw new Error("Session not found.");
@@ -47,23 +66,27 @@ export default class TelegramService {
             onError: (err) => console.log(err),
         })
 
-        this.session.save()
+        console.log("Session restored.")
+        const sstr = this.session.save()
+
+        // write string to file
+        writeFileSync(targetPath, sstr)
     }
 
 
     async downloadMediaFromMessage(mediaData: { chatId?: string | EntityLike, msgId: number}, onDownloadProgress?: (progress: BigInteger, total: BigInteger) => void) {
         const msgId = mediaData.msgId
-        const message = await this.client.getMessages(mediaData.chatId || "me", {
+        const message = await this.client?.getMessages(mediaData.chatId || "me", {
             ids: [msgId]
         })
 
         await delay(1000)
 
-        if (message.length === 0) {
+        if (!message || message?.length === 0) {
             throw new Error("Message not found.")
         }
 
-        if ((!message[0]?.media as any)?.document) {
+        if ((!message[0].media as any)?.document) {
             throw new Error("Message is not a video.")
         }
 
@@ -71,7 +94,7 @@ export default class TelegramService {
         return {
             fileName: (!message[0]?.media as any)?.document?.file_name || "video.mp4",
             mimeType: (!message[0]?.media as any)?.document?.mime_type || "mp4",
-            buffer: await this.client.downloadMedia(message[0], {
+            buffer: await this.client?.downloadMedia(message[0], {
             progressCallback: onDownloadProgress
         })}
     }
