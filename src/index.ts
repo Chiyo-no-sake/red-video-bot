df0192b31af2f15d047c29126bfa6c92f1f71b52
Pasini Luca <luca.pasini@supsi.ch>
2023-11-25 15:57:42 +0100

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,36 @@
-import * as dotenv from 'dotenv';
-import { ConfigParser } from './config/ConfigParser.js';
-import TelegramService from './tg-client/TelegramService.js';
-import { startBot } from './bot/Bot.js';
-import { OpenAIGenerator } from './name-generator/OpenAIGenerator.js';
-import EjsEngine from './template/EjsEngine.js';
-import { UIService } from './UIService.js';
-import { VideoDownloader } from './download/VideoDownloader.js';
-import { DownloadQueue } from './download/DownloadQueue.js';
+import * as dotenv from 'dotenv'
+import { ConfigParser } from './config/ConfigParser.js'
+import TelegramService from './tg-client/TelegramService.js'
+import { startBot } from './bot/Bot.js'
+import { OpenAIGenerator } from './name-generator/OpenAIGenerator.js'
+import EjsEngine from './template/EjsEngine.js'
+import { UIService } from './UIService.js'
+import { VideoDownloader } from './download/VideoDownloader.js'
+import { DownloadQueue } from './download/DownloadQueue.js'
 
 const main = async () => {
-    dotenv.config();
+  dotenv.config()
 
-    const configParser = new ConfigParser(process.env);
-    const config = configParser.parseConfig();
-    
-    console.log("Starting with config:", config)
-    const telegram = new TelegramService({
-        ...config,
-        ...config.auth,
-    });
+  const configParser = new ConfigParser(process.env)
+  const config = configParser.parseConfig()
 
+  console.log('Starting with config:', config)
+  const telegram = new TelegramService({
+    ...config,
+    ...config.auth,
+  })
 
-    await telegram.start();
-    console.log("Session restored.")
+  await telegram.start()
+  console.log('Session restored.')
 
-    const engine = new EjsEngine();
-    const ui = new UIService(engine);
-    const nameGenerator = new OpenAIGenerator(config.openai, engine);
-    const videoDownloader = new VideoDownloader(config, telegram);
-    const downloadQueue = new DownloadQueue(videoDownloader, ui);
+  const engine = new EjsEngine()
+  const ui = new UIService(engine)
+  const nameGenerator = new OpenAIGenerator(config.openai, engine)
+  const videoDownloader = new VideoDownloader(config, telegram)
+  const downloadQueue = new DownloadQueue(videoDownloader, ui)
 
-    await startBot(config, telegram, nameGenerator, downloadQueue, ui)
-    console.log("Bot started.")
+  await startBot(config, nameGenerator, downloadQueue, ui)
+  console.log('Bot started.')
 }
 
-
-main();
+main()
